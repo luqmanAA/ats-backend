5fcba51861f26f524410401c71e2124f59dc3786
Lukman Abisoye <25963466+luqmanAA@users.noreply.github.com>
2022-06-22 17:41:58 +0100

    User account (profile modification fixed)

--- week-3/03_user_account.py
+++ week-3/03_user_account.py
@@ -11,45 +11,10 @@
 import csv
 import sys
 
-def user_info():
-    pass
-
-def change_password(username, password):
-    old_password = input("Enter your old password: ")
-    while old_password == '' or old_password != password or len(old_password) <8:
-        old_password = input("Password must contain at least 8 characters, please try again: ")
-    
-    new_password = input("Enter your new password: ")
-    while new_password == '' or len(new_password) <8:
-        new_password = input("Password must contain at least 8 characters, please try again: ")
-    # while username.lower() in new_password.lower() or firstname.lower() in new_password.lower() or lastname.lower() in new_password.lower():
-    while username.lower() in new_password.lower():
-        new_password = input("Unaccepted password! Password cannot contain your username or name, please try again: ")
-    
-    new_password_confirm = input("Please confirm your new password: ")
-    while new_password_confirm != new_password:
-        new_password_confirm = input("Those passwords didn't match, please try again: ")
-
-    # password = new_password
-    # print("Password succesfully changed")
-    # return
-    data_holder = []
-    with open(r'week-3\user_accounts.csv', 'r+') as f:
-        headers = ['firstname', 'lastname', 'username', 'password']
-        data_reader = csv.DictReader(f, fieldnames=headers)
-        data_writer = csv.DictWriter(f, fieldnames=headers)
-        for data in data_reader:
-            if (username == data['username']) and (old_password == data['password']):
-                data['password'] = new_password
-        for data in data_reader:
-            if data['firstname'] != 'firstname':
-                data_writer.writerow(data)
-            
-        
+headers = ['firstname', 'lastname', 'username', 'password', 'phone_number', 'address', 'dob', 'gender']
 
 def save_data(firstname, lastname, username, password):
-    with open(r'week-3\user_accounts.csv', 'a') as f:
-        headers = ['firstname', 'lastname', 'username', 'password']
+    with open(r'week-3\user_accounts.csv', 'a+') as f:
         handler = csv.DictWriter(f, fieldnames=headers)
         # handler.writeheader()
         handler.writerow({
@@ -66,8 +31,28 @@ def retrieve_data():
     with open(r'week-3\user_accounts.csv', 'r') as f:
         handler = csv.DictReader(f)
         return (list(handler))
-            
-# def confir
+
+def modify_data(**kwargs):
+    data_holder = []
+    with open(r'week-3\user_accounts.csv', 'r+') as f:
+        data_reader = csv.DictReader(f, fieldnames=headers)
+        for data in data_reader:
+            if len(kwargs) == 2:
+                if (kwargs['username'] == data['username']):
+                    data['password'] = kwargs['password']
+            if len(kwargs) > 2:
+                if (kwargs['username'] == data['username']):
+                    data['password'] = kwargs['password']
+                    data['phone_number'] = kwargs['phone_number']
+                    data['address'] = kwargs['address']
+                    data['dob'] = kwargs['dob']
+                    data['gender'] = kwargs['gender']
+            data_holder.append(data)
+    with open(r'week-3\user_accounts.csv', 'w') as f:
+        data_writer = csv.DictWriter(f, fieldnames=headers)
+        # data_writer.writeheader()
+        for data in data_holder:
+            data_writer.writerow(data)
 
 def signin():
     username = input("Enter your username to sign-in: ")
@@ -85,11 +70,12 @@ def signin():
     # print(user_data)
 
 def signin_option(**kwargs):
+
     selected_options = input("1. Edit profile \n2. Change password \n3. Logout\n")
     if selected_options == '1':
-        return edit_profile()
+        return edit_profile(kwargs['username'], kwargs['password'])
     elif selected_options == '2':
-        return change_password(kwargs['username'], kwargs['password'])
+        return change_password(kwargs['firstname'], kwargs['lastname'], kwargs['username'], kwargs['password'])
     elif selected_options == '3':
         sys.exit()
     else:
@@ -97,6 +83,7 @@ def signin_option(**kwargs):
         return user()
 
 def signup():
+    user_data = retrieve_data()
     firstname = input("Please enter your firstname: ")
     while firstname == '':
         firstname = input("Firstname cannot be empty, please enter your firstname: ")
@@ -106,7 +93,6 @@ def signup():
     username = input("Please enter your username: ")
     while username == '' or len(username) < 6:
         username = input("Username not accepted, username must contain atleast 6 characters: ")
-    
     password = input("Enter your password: ")
     while password == '' or len(password) <8:
         password = input("Password must contain at least 8 characters, please try again: ")
@@ -125,8 +111,49 @@ def signup():
             }
     save_data(**user_data)
 
-def edit_profile():
-    pass
+
+def change_password(firstname, lastname, username, password):
+    old_password = input("Enter your old password: ")
+    while old_password == '' or old_password != password or len(old_password) <8:
+        old_password = input("Password must contain at least 8 characters, please try again: ")
+    
+    new_password = input("Enter your new password: ")
+    while new_password == '' or len(new_password) <8:
+        new_password = input("Password must contain at least 8 characters, please try again: ")
+    # while username.lower() in new_password.lower() or firstname.lower() in new_password.lower() or lastname.lower() in new_password.lower():
+    while username.lower() in new_password.lower() or firstname.lower() in new_password.lower() or lastname.lower() in new_password.lower():
+        new_password = input("Unaccepted password! Password cannot contain your username or name, please try again: ")
+    
+    new_password_confirm = input("Please confirm your new password: ")
+    while new_password_confirm != new_password:
+        new_password_confirm = input("Those passwords didn't match, please try again: ")
+
+    modify_data(**{'username': username, 'password': new_password})
+    
+    print("Password succesfully changed")
+    return signin_option()
+            
+
+def edit_profile(username, password):
+    phone_number = input("Please enter your phone number (required): ")
+    while phone_number == '' or not phone_number.isnumeric() or len(phone_number) < 11:
+        phone_number = input("Phone number is required, please enter your phone number: ")
+    address = input("Please enter your address: ")
+    dob = input("Please enter your date of birth: ")
+    gender = input("Please enter your gender (m/f): ")
+    while gender == '' or not gender.isalpha() or not (gender == 'm' or gender == 'f'):
+        gender = input("Invalid gender, please enter your gender: ")
+    user_info = {
+        'username': username,
+        'password': password,
+        'phone_number': phone_number,
+        'address': address,
+        'gender': gender,
+        'dob': dob
+    }
+    modify_data(**user_info)
+    print("Profile updated successfully!")
+    return signin_option()
 
 
 def user():
